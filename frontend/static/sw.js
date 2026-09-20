57d543909b6a4aa0a0e75156bbc2ee1c37aaef2f
Bruno Novaes <brunovivo26@gmail.com>
2026-09-20 02:07:00 +0100

    Corrige logo 3D e icones do Seringa

--- frontend/static/sw.js
+++ frontend/static/sw.js
@@ -1,12 +1,12 @@
-const CACHE = 'seringa-v13-png-logo';
+const CACHE = 'seringa-v14-3d-png-logo';
 const BASE = './';
 const ASSETS = [
   BASE,
   './index.html',
-  './logo-seringa-3d.svg?v=20260920',
-  './icon-192.png?v=20260920',
-  './icon-512.png?v=20260920',
-  './apple-touch-icon.png?v=20260920',
+  './icon-512.png?v=20260921',
+  './icon-192.png?v=20260921',
+  './icon-512.png?v=20260921',
+  './apple-touch-icon.png?v=20260921',
   './manifest.webmanifest',
   './inicio.html',
   './app.html',
